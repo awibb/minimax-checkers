d6600909baa10f001b6630f99873d44fb5ccb2a1
Andrew <andrew_wibbenmeyer@outlook.com>
2022-04-19 16:06:48 -0500

    removed unneeded variable

--- piece.py
+++ piece.py
@@ -1,9 +1,8 @@
 import pygame
-from const import SQUARE_SIZE, GRAY
+from const import SQUARE_SIZE
 
 class Piece:
-    PADDING = 15
-    BORDER = 2
+    PADDING = 10
     def __init__(self, row, col, color):
         self.row = row
         self.col = col
@@ -11,12 +10,11 @@ class Piece:
         self.x, self.y = 0,0
         self.piece_position()
     
-
+   
     def piece_position(self):
         self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
         self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
 
     def draw(self, window):
         radius = SQUARE_SIZE // 2 - self.PADDING
-        
         pygame.draw.circle(window, self.color, (self.x, self.y), radius)
