34a8bb4600e9a6dc87aca4c0b820010ae8c52476
Andrew <andrew_wibbenmeyer@outlook.com>
2022-04-19 16:05:05 -0500

    added the ability for the valid moves to be drawn on the ui

--- board.py
+++ board.py
@@ -1,5 +1,5 @@
 import pygame 
-from const import BLACK,ROWS, SQUARE_SIZE, RED, COLS, WHITE
+from const import BLACK,ROWS, SQUARE_SIZE, RED, COLS, WHITE, GREEN
 from piece import Piece
 
 class Board:
@@ -7,6 +7,12 @@ class Board:
         self.board = []
         self.create_board()
     
+
+    def valid_move_position(self, col, row):
+        #takes the row and col position of a value in the 2d board array and converts it to the position of that square on the ui
+        return SQUARE_SIZE * col + SQUARE_SIZE // 2, SQUARE_SIZE * row + SQUARE_SIZE // 2
+
+
     def draw_squares(self, window):
         #black background
         window.fill(BLACK)
@@ -35,9 +41,29 @@ class Board:
             order = not order
 
     def draw(self, window):
+        #loops through the board and if the current index board[row][col] is a piece it will be drawn on the ui, if board[row][col] = 1 a green circle will be drawn in the position of board[row][col] on the ui
         self.draw_squares(window)
         for row in range(ROWS):
             for col in range(COLS):
                 piece = self.board[row][col]
                 if(piece != 0):
-                    piece.draw(window)
+                    if piece == 1:
+                        pygame.draw.circle(window, GREEN, (self.valid_move_position(col,row)), 15)
+                    else:
+                        piece.draw(window)
+
+    def find_valid_move(self):
+        #hard coded for a visualization but this needs to be implemented
+        self.board[4][1] = 1
+        self.board[4][3] = 1
+
+    def clear_moves(self):
+        #after the move has been processed the board needs to have all green circles removed from it which are represented in the board as 1's
+         for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece == 1:
+                    self.board[row][col] = 0
+
+
+        
