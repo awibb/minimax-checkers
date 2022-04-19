b41e09f1fd0ce465733b14d27d1cc00f2ecf42a1
Andrew <andrew_wibbenmeyer@outlook.com>
2022-04-19 16:06:09 -0500

    Added Green to use to display valid moves. Updated documentation

--- const.py
+++ const.py
@@ -1,9 +1,14 @@
-import pygame
-
-WIDTH, HEIGHT = 800,800
+#size of the window
+WIDTH, HEIGHT = 600,600
+#dimensions of the game board
 ROWS, COLS = 8,8
-SQUARE_SIZE = 800//ROWS
+#size of each square on the ui
+SQUARE_SIZE = 600//ROWS
+#player piece color
 RED = (255,0,0)
+#ai piece color
 WHITE = (255,255,255)
+#Background Color
 BLACK = (0,0,0)
-GRAY = (128,128,128)
+#Used for drawing valid moves of a selected piece
+GREEN = (0,255,0)
